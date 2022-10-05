10bdb684f537ce8b4e1924f60d48b36ac99ad6d7
17101934ksy <ksyn1611@naver.com>
2022-10-05 21:38:29 +0900

    yml test

---
+++ kakaoToy1/src/main/java/kakaoToy1/kakaoToy1/domain/member/TimeEntity.java
@@ -0,0 +1,7 @@
+package kakaoToy1.kakaoToy1.domain.member;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class TimeEntity {
+}
---
+++ springlogintest/src/main/java/springlogintest/springlogintest/SpringlogintestApplication.java
@@ -0,0 +1,13 @@
+package springlogintest.springlogintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlogintestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlogintestApplication.class, args);
+	}
+
+}
